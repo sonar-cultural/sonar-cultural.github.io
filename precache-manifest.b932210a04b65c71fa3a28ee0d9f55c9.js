e8981e53877772d5962dc819cd89b09ae259b191
Anna Gabriela <agmc0997@gmail.com>
2019-03-15 03:10:21 -0300

    Initial dist subtree commit

---
+++ precache-manifest.b932210a04b65c71fa3a28ee0d9f55c9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "de6ac04aefd0039e6f735ec717eac72a",
+    "url": "/img/livia.de6ac04a.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d026092abfe6577fc3c79207382456c7",
+    "url": "/img/vivi.d026092a.jpg"
+  },
+  {
+    "revision": "c62e28f2e3136a4357e7",
+    "url": "/css/chunk-vendors.e00a3e23.css"
+  },
+  {
+    "revision": "b5bc0a976023678d12d3ad948fb56009",
+    "url": "/img/processo.b5bc0a97.png"
+  },
+  {
+    "revision": "e2a3cd79ab0c20bb3bc6508d9df5cab5",
+    "url": "/img/git.e2a3cd79.png"
+  },
+  {
+    "revision": "a4c66a6fbc27db5674d32fbee2c8dbdd",
+    "url": "/img/tiago.a4c66a6f.jpg"
+  },
+  {
+    "revision": "44de942668a76e8073302572be66c5ee",
+    "url": "/img/wilson.44de9426.jpg"
+  },
+  {
+    "revision": "6e9b27a3c750b33d7173342e677c71dc",
+    "url": "/img/whatsapp.6e9b27a3.png"
+  },
+  {
+    "revision": "ba92442cfccc93574074",
+    "url": "/js/app.9321f09c.js"
+  },
+  {
+    "revision": "c62e28f2e3136a4357e7",
+    "url": "/js/chunk-vendors.f7ea087c.js"
+  },
+  {
+    "revision": "bdd25ad520005abbe902a7d0a28e3b63",
+    "url": "/img/drive.bdd25ad5.png"
+  },
+  {
+    "revision": "1f503c157d67177beaa570f1d869d644",
+    "url": "/img/logo.1f503c15.png"
+  },
+  {
+    "revision": "10847ae83151cf052ec74dae67ce739f",
+    "url": "/img/lucca.10847ae8.jpg"
+  },
+  {
+    "revision": "00166fceb9af1b6606cc8fd0d5ce227a",
+    "url": "/img/banner.00166fce.png"
+  },
+  {
+    "revision": "25a2e83b674fcfe004379ae265e81aa8",
+    "url": "/img/anna.25a2e83b.jpg"
+  },
+  {
+    "revision": "1ccd25225ca19c89f0ef6cd57a08c07c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba92442cfccc93574074",
+    "url": "/css/app.e06f432d.css"
+  }
+];
