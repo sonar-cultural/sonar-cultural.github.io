5651708dad284feac9baed2bbbd75e51fdb571d2
Anna Gabriela <agmc0997@gmail.com>
2019-04-30 00:56:47 -0300

    add team's responsabilities

--- src/views/Papeis.vue
+++ src/views/Papeis.vue
@@ -1,5 +1,15 @@
 <template>
   <Page title="Papéis e Responsabilidades">
+    <h2>Desenvolvedor(a)</h2>
+    <hr>
+    <p>Responsável por definir e transformar os conjunto de requisitos
+      do projeto em código funcional desenvolvendo um produto
+      de acordo com a expectativa do cliente.</p><br>
+    <h2>Designer</h2>
+    <hr>
+    <p>Responsável por pesquisar, idealizar, analisar e realizar testes
+      com os protótipos.
+    </p>
   </Page>
 </template>
 
