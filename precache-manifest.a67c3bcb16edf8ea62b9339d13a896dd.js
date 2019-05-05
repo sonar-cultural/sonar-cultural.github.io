a3dbdd368ed787490365a484678a8f7341564923
Anna Gabriela <agmc0997@gmail.com>
2019-05-05 20:19:36 -0300

    new site

---
+++ precache-manifest.a67c3bcb16edf8ea62b9339d13a896dd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e9b27a3c750b33d7173342e677c71dc",
+    "url": "/img/whatsapp.6e9b27a3.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "de6ac04aefd0039e6f735ec717eac72a",
+    "url": "/img/livia.de6ac04a.jpg"
+  },
+  {
+    "revision": "ec89cc1d5298f4c4a821",
+    "url": "/css/chunk-vendors.f30cb3dc.css"
+  },
+  {
+    "revision": "5f3041b3388a6972811d08c483edc1a4",
+    "url": "/img/trello.5f3041b3.png"
+  },
+  {
+    "revision": "b5bc0a976023678d12d3ad948fb56009",
+    "url": "/img/processo.b5bc0a97.png"
+  },
+  {
+    "revision": "e2a3cd79ab0c20bb3bc6508d9df5cab5",
+    "url": "/img/git.e2a3cd79.png"
+  },
+  {
+    "revision": "a4c66a6fbc27db5674d32fbee2c8dbdd",
+    "url": "/img/tiago.a4c66a6f.jpg"
+  },
+  {
+    "revision": "44de942668a76e8073302572be66c5ee",
+    "url": "/img/wilson.44de9426.jpg"
+  },
+  {
+    "revision": "4073a61aea1786f770613bf50e65370c",
+    "url": "/img/team.4073a61a.jpg"
+  },
+  {
+    "revision": "c0e3867214df850773ec",
+    "url": "/js/app.e98f0b1f.js"
+  },
+  {
+    "revision": "ec89cc1d5298f4c4a821",
+    "url": "/js/chunk-vendors.194001ba.js"
+  },
+  {
+    "revision": "d026092abfe6577fc3c79207382456c7",
+    "url": "/img/vivi.d026092a.jpg"
+  },
+  {
+    "revision": "bdd25ad520005abbe902a7d0a28e3b63",
+    "url": "/img/drive.bdd25ad5.png"
+  },
+  {
+    "revision": "ef77da19090a1528e760a9e02cb9592b",
+    "url": "/img/logo.ef77da19.png"
+  },
+  {
+    "revision": "ab1dc39cdb37feb05417ecbe5a212ba6",
+    "url": "/img/Workflow.ab1dc39c.png"
+  },
+  {
+    "revision": "10847ae83151cf052ec74dae67ce739f",
+    "url": "/img/lucca.10847ae8.jpg"
+  },
+  {
+    "revision": "25a2e83b674fcfe004379ae265e81aa8",
+    "url": "/img/anna.25a2e83b.jpg"
+  },
+  {
+    "revision": "ee7550e1c9f3ea7006fa23783c6cb23f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e3867214df850773ec",
+    "url": "/css/app.3c107284.css"
+  }
+];
